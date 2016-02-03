d3a9fbd3bf8ee8cba8f532cd146704763627e766
Alana Alexander <laninsky@gmail.com>
2016-02-03 13:10:02 -0600

    Update missingsummary_loci.R

--- missingsummary_loci.R
+++ missingsummary_loci.R
@@ -1,28 +1,19 @@
 intable <- read.table("summary",header=FALSE,stringsAsFactors=FALSE,sep=" ")
+locinames <- read.table("fasta_names",header=FALSE,stringsAsFactors=FALSE,sep=" ")
 
 #Make this the actual number of taxa you have (not 2N)
-no_taxa <- 24
+no_taxa <- 23
+noloci <- dim(locinames)[1]
 
-ncols <- ((dim(intable)[1])/no_taxa)+1
+outtable <- matrix(NA,ncol=2,nrow=noloci)
+outtable[,1] <- locinames[,1]
 
-to_record <- matrix(NA,ncol=ncols,nrow=no_taxa)
-
-to_record[,1] <- intable[1:no_taxa,1]
-to_record[,2] <- intable[1:no_taxa,2]
-
-for (j in 3:ncols) {
-begin <- ((j-2)*no_taxa)+1
-endit <- begin+no_taxa-1
-to_record[,j] <- intable[begin:endit,2]
+for (i in 1:noloci) {
+beginning <- (i-1)*no_taxa+1
+end <- beginning+no_taxa-1
+outtable[i,2] <- sum(intable[beginning:end,2])
 }
 
-rm(intable)
-
-for (j in 1:no_taxa) {
-printmean <- mean(as.numeric(to_record[j,2:ncols]))
-printsd <- sd(as.numeric(to_record[j,2:ncols]))
-toprint <- paste("For ",to_record[j,1],": mean=",printmean,", sd=",printsd,sep="")
-print(noquote(toprint))
-}
+write.table(outtable, "locus_missing_data.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
 q()
