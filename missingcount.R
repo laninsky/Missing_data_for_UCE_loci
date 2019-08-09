2846bb558ce3e9dbbba6b85b5e906d07dc46e5b9
Alana Alexander <laninsky@gmail.com>
2019-08-09 12:04:36 +1200

    Update missingcount.R
    changed so that missingcount.R does the function of the bash script

--- missingcount.R
+++ missingcount.R
@@ -1,39 +1,39 @@
 library("tidyverse")
 
-# Read in the data, get the total number of rows
-intable <- read_table("count",col_names=FALSE)
-rows <- dim(intable)[1]
+# List of fasta files in directory
+fastafiles <- list.files(pattern="fasta")
 
-# Get the number of taxa in the fasta file
-notaxa <- (intable %>% filter(grepl(">",X1)) %>% dim())[1]
+# Looping through each fasta file
+for (i in fastafiles) {
 
-# Set up the intermediate matrix to calculate the missing data off 
-onelined <- matrix(NA,ncol=2,nrow=notaxa)
-onelined[,1] <- as.matrix(intable %>% filter(grepl(">",X1)))
-sequencepaste <- NULL
-onelined_rowindex <- 1
+  # Read in the data, get the total number of rows
+  intable <- read_table(i,col_names=FALSE)
+  rows <- dim(intable)[1]
 
-# Filling in the intermediate matrix with the sequence data
-for (j in 2:rows) {
-  if ((length(grep(">",intable[j,1])))>0) {
-    onelined[onelined_rowindex,1] <- str_count(sequencepaste,fixed("?")) + str_count(sequencepaste,fixed("-"))
-    onelined_rowindex <- onelined_rowindex + 1
-    sequencepaste <- NULL
-  } else {
-    sequencepaste <- paste(sequencepaste,intable[j,1],sep="")
+  # Get the number of taxa in the fasta file
+  notaxa <- (intable %>% filter(grepl(">",X1)) %>% dim())[1]
+
+  # Set up the intermediate matrix to calculate the missing data off 
+  onelined <- matrix(NA,ncol=2,nrow=notaxa)
+  onelined[,1] <- as.matrix(intable %>% filter(grepl(">",X1)))
+  sequencepaste <- NULL
+  onelined_rowindex <- 1
+
+  # Filling in the intermediate matrix with the sequence data
+  for (j in 2:rows) {
+    if ((length(grep(">",intable[j,1])))>0) {
+      onelined[onelined_rowindex,2] <- str_count(sequencepaste,fixed("?")) + str_count(sequencepaste,fixed("-"))
+      onelined_rowindex <- onelined_rowindex + 1
+      sequencepaste <- NULL
+    } else {
+      sequencepaste <- paste(sequencepaste,intable[j,1],sep="")
+    }
   }
+
+  onelined[(dim(onelined)[1]),2] <- str_count(sequencepaste,fixed("?")) + str_count(sequencepaste,fixed("-"))
+  onelined <- cbind(onelined,i)
+    
+  write.table(onelined, "summary",quote=FALSE, col.names=FALSE,row.names=FALSE,append=TRUE)
 }
-
-onelined[(notaxa*2),1] <- sequencepaste
-
-
- str_count(to_write[(x+1),1],fixed("-"))
-x <- x + 2
-j <- j + 1
-}
-
-to_write_temp <- to_write_temp[order(to_write_temp[,1]),]
-
-write.table(to_write_temp, "summary",quote=FALSE, col.names=FALSE,row.names=FALSE,append=TRUE)
-
+  
 q()
