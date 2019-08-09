b2ae8ef23e4a75f22d7aa4c71c1b6551d5c8bdba
Alana Alexander <laninsky@gmail.com>
2019-08-09 12:47:02 +1200

    Update missingsummary_sample.R

--- missingsummary_sample.R
+++ missingsummary_sample.R
@@ -5,8 +5,12 @@ library("tidyverse")
 intable <- read_table2("summary",col_names=FALSE)
 
 # Summarizing the data
-outtable <- intable %>% group_by(X1) %>% summarise(mean_missing_bp=mean(X2),sd_missing_bp=sd(X2))
-names(outtable) <- c("Sample name","Mean missing data (bp)","SD missing data (bp)")
+no_loci <- length(as.matrix(unique(intable$X3)))
+
+outtable <- intable %>% group_by(X1) %>% 
+  summarise(mean_missing_bp=mean(X2),sd_missing_bp=sd(X2),no_missing_taxa=(no_loci-length(unique(X3))))
+
+names(outtable) <- c("Locus name","Mean missing data (bp)","SD missing data (bp)", "Number of missing loci")
 
 # Write out the table
-write_delim(outtable,"missing_data_by_sample.txt")
+write_delim(outtable,"missing_data_by_taxa.txt")
